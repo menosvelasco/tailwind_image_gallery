b13dd7019ab46f3a9835ea1b6d6d14ea076aec65
Menos  Velasco jr <menozvelasco1@gmail.com>
2021-02-11 15:53:21 -0800

    complete project search button need color

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
+
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [term]);
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg
-">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-300 text-xl mb-2">
-          Photo by John Smith
-        </div>
-        <ul>
-          <li>
-            <strong>View: </strong>
-            5000
-          </li>
-          <li>
-            <strong>Dowloads: </strong>
-            3000
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            58050
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-sans text-black-300 mr-2">#tag1</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-sans text-black-300 mr-2">#tag2</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-sans text-black-300 mr-2">#tag3</span>
-      </div>
+    <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
